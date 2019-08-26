d5fbbf8c434b7e87eb60e7515f289ffe524c1d2a
mangakrack <51804124+mangakrack@users.noreply.github.com>
2019-08-25 21:24:36 -0300

    Sharingan
    This script was an information gathering

---
+++ information gathering.py
@@ -0,0 +1,41 @@
+import whois
+import os
+import requests
+from termcolor import cprint 
+cprint('''
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+ _____ _                _                         
+/  ___| |              (_)                        
+\ `--.| |__   __ _ _ __ _ _ __   __ _  __ _ _ __  
+ `--. \ '_ \ / _` | '__| | '_ \ / _` |/ _` | '_ \ 
+/\__/ / | | | (_| | |  | | | | | (_| | (_| | | | |
+\____/|_| |_|\__,_|_|  |_|_| |_|\__, |\__,_|_| |_|
+                                 __/ |            
+                                |___/             
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+                                                  
+''','red')
+#whois
+web = input ("dame el url de la pagina:")
+whois=whois.query (web)
+print (whois.__dict__)
+#ping
+hostname = (web)
+response = os.system("ping -c 1 " + hostname)
+#nslookup
+os.system("nslookup " + hostname)
+#nmap
+os.system("nmap -sV --script=banner " + hostname)
